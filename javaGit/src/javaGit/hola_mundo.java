f1b866f115687b2a6ae9ab86bc5b2e555b6d8204
Jesús María Olalla Pérez <jesusm.olalla.alu@iescampanillas.com>
2021-03-03 04:28:03 +0100

    Nuevo cambio

--- javaGit/src/javaGit/hola_mundo.java
+++ javaGit/src/javaGit/hola_mundo.java
@@ -1,11 +1,22 @@
 package javaGit;
-
+import java.util.Scanner;
 public class hola_mundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("Hola, me llamo Jesús y esto es una prueba ");
-
+		
+		System.out.print("¿Cuántas subidas a Github quieres que haga?: ");
+		int numero = sc.nextInt();
+		
+		if(numero == 2) {
+			System.out.print("De acuerdo");
+			
+		}else {
+			System.out.print("Yo creo que son demasiadas");
+		}
+		
 	}
 
 }
